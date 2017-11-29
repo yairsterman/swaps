cd0a7306a61ade5b4687ef601074bbac73a0d160
Damir <damirbar@gmail.com>
2017-11-29 21:25:11 +0200

    - Added review directive. - Fixed deviation of review. *** index.html changed! DO NOT MERGE YET ***

---
+++ site/public/directives/reviews.js
@@ -0,0 +1,14 @@
+swapsApp.directive('reviewElm', function() {
+    return {
+        restrict: 'E',
+        controller: swapperReview,
+        scope: {
+            commenter: '='
+        },
+        templateUrl: '../directives/reviews.html'
+    }
+});
+
+function swapperReview($scope, $rootScope, $location) {
+
+}
