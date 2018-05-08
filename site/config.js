c48f34dc1eb8722f351badae200c8b9349754ec9
yairsterman <yair.sterman@gmail.com>
2018-05-08 11:14:21 +0300

    google id and secret

--- site/config.js
+++ site/config.js
@@ -3,7 +3,10 @@ let config = {
     tranzillaSupplier: 'ttxswapstok',
     TranzilaPW: 't97lyt',
     CreditPass: 'TxVyPB',
-    tranzilaRequestUrl: `https://secure5.tranzila.com/cgi-bin/tranzila71pme.cgi`
+    tranzilaRequestUrl: `https://secure5.tranzila.com/cgi-bin/tranzila71pme.cgi`,
+
+    googleClientId: '678609024087-1c99lheaitodb9c8bv13ptctlj3fh2t4.apps.googleusercontent.com',
+    googleClientSecret: 'mJXTYyWlF-QeizGN6gWGIlF2'
 }
 
 module.exports = config;
