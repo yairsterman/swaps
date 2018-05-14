47a01ea554299389bab43a52460debdf622d4f7b
yairsterman <yair.sterman@gmail.com>
2018-05-14 17:52:30 +0300

    .

--- site/config.js
+++ site/config.js
@@ -12,11 +12,11 @@ let config = {
     FACEBOOK_APP_ID : '1628077027210389',
     FACEBOOK_APP_SECRET : '5a927f2caa3f5eb9a2eeaad0eaf1b225',
 
-    mongoUrl: 'mongodb://18.221.167.219/test', // for dev
-    // mongoUrl: 'mongodb://127.0.0.1/test', // for production
+    // mongoUrl: 'mongodb://18.221.167.219/test', // for dev
+    mongoUrl: 'mongodb://127.0.0.1/test', // for production
 
-    // baseUrl: 'https://swapshome.com'
-    baseUrl: 'http://localhost:3000'
+    baseUrl: 'https://swapshome.com'
+    // baseUrl: 'http://localhost:3000'
 };
 
 module.exports = config;
