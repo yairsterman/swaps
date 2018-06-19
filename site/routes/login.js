982db9fae7e8d680e7227071d9c953d622bb6f1a
stermaneran <stermaneran@gmail.com>
2018-06-19 16:13:31 +0300

    .

--- site/routes/login.js
+++ site/routes/login.js
@@ -1,6 +1,9 @@
 let express = require('express');
 let router = express.Router();
 let passport = require('passport');
+let User = require('../models/User.js');
+const bcrypt = require('bcrypt-nodejs');
+
 
 
 router.get('/facebook',
@@ -25,11 +28,43 @@ router.get('/google/callback',
 
 
 router.post('/signup',
-    passport.authenticate('local-signup', {failureRedirect: '/'}), function (req, res) {
+    passport.authenticate('local-signup', {failureRedirect: '/auth/signup'}), function (req, res) {
         // Successful authentication, redirect home.
         res.json({userId: req.session.passport.user});
     }
 );
+
+router.post('/signin',function (req, res) {
+    if(!req.body.email){
+        res.status(404).json({message: 'no email '});
+    }
+    else {
+        User.findOne({email: req.body.email},function (err, user) {
+            if(err){
+                console.log(err);
+                res.status(409).json({message: err});
+            }
+            else{
+                if(user && user.password){
+                    if (bcrypt.compareSync(req.body.password, user.password)) {
+                        res.status(200).json(user);
+                    }
+                    else{
+                        res.status(404).json({message: 'wrong password '+ req.body.password});
+                    }
+                }
+                else{
+                    if(user.password) {
+                        res.status(404).json({message: 'no user with email ' + req.body.email});
+                    }
+                    else {
+                        res.status(404).json({message: 'user ' + req.body.email + ' has no password'});
+                    }
+                }
+            }
+        })
+    }
+    });
 
 
 
