4bb492dd658e29d6769cf51549123a45b8570450
yairsterman <yair.sterman@gmail.com>
2018-07-08 14:24:06 +0300

    .

--- site/public/bower_components/daterangepicker/update-bower-version.js
+++
@@ -1,9 +0,0 @@
-// https://qubyte.codes/blog/tip-customizing-npm-version
-
-var fs = require('fs');
-var bowerJsonPath = require.resolve('./bower');
-var bowerJson = require(bowerJsonPath);
-
-bowerJson.version = process.env.npm_package_version; // npm injects this
-
-fs.writeFileSync(bowerJsonPath, JSON.stringify(bowerJson, null, 2));
