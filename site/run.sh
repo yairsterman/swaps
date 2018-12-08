6fec834401fdab3ebe331693318884a30fb16f16
yairsterman <yair.sterman@gmail.com>
2018-12-08 20:44:59 +0200

    .

--- site/run.sh
+++ site/run.sh
@@ -1,5 +1,5 @@
 #sudo mongod --fork --logpath /var/log/mongod.log --dbpath /home/ec2-user/data
 #sudo mongod --dbpath /home/ec2-user/data --shutdown
 forever stop api
-forever --uid api start app.js
+forever --uid -a api start app.js
 
